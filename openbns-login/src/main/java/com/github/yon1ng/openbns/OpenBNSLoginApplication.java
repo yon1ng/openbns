7f0b1bdd05ee41ed40246600e4902a924cc272fa
xbcsl <41776382+xbcsl@users.noreply.github.com>
2023-07-24 17:57:22 +0800

    init

---
+++ openbns-login/src/main/java/com/github/yon1ng/openbns/OpenBNSLoginApplication.java
@@ -0,0 +1,16 @@
+package com.github.yon1ng.openbns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ：yon1ng
+ * @date ：Created in 2023/7/24 17:22
+ * @description：
+ */
+@SpringBootApplication
+public class OpenBNSLoginApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OpenBNSLoginApplication.class, args);
+    }
+}
